b9e604ccdd9e729dcec373fc23da541b85505fb1
vesajoo <joonasvesa0@gmail.com>
2023-12-12 06:28:21 +0200

    Added frontend

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
+app.use(express.static('dist'))
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
